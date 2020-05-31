ebbb9d93cbdaa2ccaffe01aa2244247083c84fcc
Jordan Speers <jspeers615@live.co.uk>
2020-05-31 19:06:56 +0100

    Initial commit of docker multi service architecture fibonacci calculator

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            Hello world
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
